936623dfbed6e10477ff12b4afd222eda2c3e2c1
pc <pc@DESKTOP-O9GQ58M>
2021-08-09 22:04:38 +0900

    시작

---
+++ myfatherProject/src/main/java/com/example/myfatherProject/MyfatherProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfatherProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfatherProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfatherProjectApplication.class, args);
+	}
+
+}
---
+++ myfatherProject/src/test/java/com/example/myfatherProject/MyfatherProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfatherProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfatherProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
